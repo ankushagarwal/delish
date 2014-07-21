9e88db4d05134b4c14ae874df93e529f0236fadd
Ankush Agarwal <ankushagarwal11@gmail.com>
2014-07-21 01:19:26 -0700

    JSLint and increase timeout

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-setTimeout(function() {
-  if (confirm("Save ? ") == true) {
-    $('.tag-suggestion-item').each(function() {
-      $(this).click();
-    });
-    $('.text-btn-primary').click();
-    setTimeout(function() {
-      window.close()
-    }, 1200);
+setTimeout(function () {
+  if ($('body').text().indexOf("You saved this link on") === -1) {
+    if (confirm("Save ? ") === true) {
+      $('.tag-suggestion-item').each(function () {
+        $(this).click();
+      });
+      $('.text-btn-primary').click();
+      setTimeout(function () {
+        window.close();
+      }, 1200);
+    }
   }
-}, 850);
+}, 1000);
