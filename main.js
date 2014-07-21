c6062367d72adf7c41c0ffccc9bd3ee6dcb58418
Ankush Agarwal <ankushagarwal11@gmail.com>
2014-07-20 21:20:49 -0700

    Format JavaScript code

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-setTimeout(function () {
-    if (confirm("Save ? ") == true) {
-        $('.tag-suggestion-item').each(function () {
-            $(this).click();
-        });
-        $('.text-btn-primary').click();
-        setTimeout(function () {
-            window.close()
-        }, 1200);
-    }
+setTimeout(function() {
+  if (confirm("Save ? ") == true) {
+    $('.tag-suggestion-item').each(function() {
+      $(this).click();
+    });
+    $('.text-btn-primary').click();
+    setTimeout(function() {
+      window.close()
+    }, 1200);
+  }
 }, 850);
